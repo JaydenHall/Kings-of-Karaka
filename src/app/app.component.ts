559c3162076781a37af3697e08ed58ac83c80060
JaydenHall <jaydenhallpt@hotmail.co.nz>
2022-04-01 16:41:20 +1300

    ready to load

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,81 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  template: `
+  <div>
+    <div class="container">
+      <img class="pic" src="assets/images/hhh.png">
+      <div class="bio">
+        <div class="name">Name: Jayden</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <img class="pic" src="assets/images/john.png">
+      <div class="bio">
+        <div class="name">Name: Tom</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <img class="pic" src="assets/images/james.png">
+      <div class="bio">
+        <div class="name">Name: Jono</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <img class="pic" src="assets/images/scottie.png">
+      <div class="bio">
+        <div class="name">Name: Lee</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <img class="pic" src="assets/images/ray.png">
+      <div class="bio">
+        <div class="name">Name: Ryan</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>    
+    <div class="container">
+      <img class="pic" src="assets/images/tomas.png">
+      <div class="bio">
+        <div class="name">Name: Sam</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <img class="pic" src="assets/images/david.png">
+      <div class="bio">
+        <div class="name">Name: Rob</div>
+        <div class="results">
+          <div class="wins">Winners: 0</div>
+          <div class="loses">Losers: 0</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  `
 })
 export class AppComponent {
   title = 'kings-of-karaka';
